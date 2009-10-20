3992ae3b5b6565539144413e2cba7c1decc1f9aa
Chris Dent <cdent@host57.msm.che.vodafone>
2009-10-20 10:52:09 +0100

    Tidy up setup.py a bit more.

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ Setup.py for tiddlyweb-plugins
 import os
 from setuptools import setup, find_packages
 
-VERSION = 0.9
+VERSION = '0.10'
 
 setup(
-        name = 'tiddlywebplugins.utils',
         namespace_packages = ['tiddlywebplugins'],
+        name = 'tiddlywebplugins.utils',
         long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
         version = VERSION,
         description = 'Tools and methods for managing TiddlyWeb plugins',
         author = 'Chris Dent',
         author_email = 'cdent@peermore.com',
-        url = 'http://pypi.python.org/pypi/tiddlywebplugins',
+        url = 'http://pypi.python.org/pypi/tiddlywebplugins.utils',
         packages = find_packages(),
         platforms = 'Posix; MacOS X; Windows',
         install_requires = ['setuptools', 'tiddlyweb'],
