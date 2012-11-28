a82ff168ac4220831a3bcafeae72027b79a2d7cc
Chris Dent <chris.dent@gmail.com>
2012-11-28 18:26:24 +0000

    release 1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Setup.py for tiddlywebplugins.utils
 import os
 from setuptools import setup, find_packages
 
-VERSION = '1.5'
+VERSION = '1.6'
 
 setup(
         namespace_packages = ['tiddlywebplugins'],
