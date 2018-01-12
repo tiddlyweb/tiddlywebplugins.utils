1c44dbd6994d8521fb8d0b0ed772caeaf20e252d
Chris Dent <chris.dent@gmail.com>
2018-01-12 10:10:50 +0000

    release 1.10
    Some python3 related cleanups were made quite a while ago
    but never released.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Setup.py for tiddlywebplugins.utils
 import os
 from setuptools import setup, find_packages
 
-VERSION = '1.9'
+VERSION = '1.10'
 
 setup(
         namespace_packages = ['tiddlywebplugins'],
